56910dffa5f5b1a8d2450d968ae1e832cbbfb81d
Robert Ilievski <rilievski54@gmail.com>
2021-09-18 18:58:27 +0200

    Created backend modules

---
+++ car-auction-backend/auction-management/src/main/java/com/example/auctionmanagement/AuctionManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.auctionmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AuctionManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AuctionManagementApplication.class, args);
+    }
+
+}
